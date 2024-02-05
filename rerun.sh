ced45013d79aefd44f50b81fcdaacf59b9ad04af
Andrei <soprachev@mail.ru>
2024-02-05 19:25:11 +0300

    dev env

--- rerun.sh
+++ rerun.sh
@@ -1,3 +1,7 @@
-docker compose down
+docker compose -p prod down
 docker compose pull
-docker compose up -d --remove-orphans
+docker compose -p prod -f docker-compose.yaml -f docker-compose.prod.yaml up -d --remove-orphans
+
+docker compose -p dev down
+docker compose pull
+docker compose -p dev -f docker-compose.yaml -f docker-compose.dev.yaml up -d --remove-orphans
