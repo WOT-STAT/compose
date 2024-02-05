52948cb9acf7cbb886e4360ce6ebebf3c82e451f
Andrei <soprachev@mail.ru>
2024-02-06 02:59:08 +0300

    add ;

--- rerun-prod.sh
+++ rerun-prod.sh
@@ -1,3 +1,3 @@
-docker compose -p prod down
-docker compose pull
-docker compose -p prod -f docker-compose.yaml -f docker-compose.prod.yaml up -d --remove-orphans
+docker compose -p prod down;
+docker compose pull;
+docker compose -p prod -f docker-compose.yaml -f docker-compose.prod.yaml up -d --remove-orphans;
