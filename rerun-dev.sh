3a17725f7e0d1b18e23f843fb79fc151d4528af9
Andrei <soprachev@mail.ru>
2024-02-06 03:50:11 +0300

    change pull

--- rerun-dev.sh
+++ rerun-dev.sh
@@ -1,3 +1,3 @@
 docker compose -p dev down;
-docker compose pull;
+docker compose -p dev -f docker-compose.yaml -f docker-compose.dev.yaml pull;
 docker compose -p dev -f docker-compose.yaml -f docker-compose.dev.yaml up -d --remove-orphans;
